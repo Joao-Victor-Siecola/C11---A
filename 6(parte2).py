98d53bf771506ad6cc55247c55ed92bdda65a8e1
João Victor Siécola Souza <jvbala2014@gmail.com>
2026-04-22 18:41:16 -0300

    Update 6(parte2).py

--- 6(parte2).py
+++ 6(parte2).py
@@ -7,23 +7,23 @@ nome = list(uploaded.keys())[0]
 
 df = pd.read_csv(nome, delimiter=';', encoding='utf-8')
 
-# Tirando espaços extras
+
 df['Location'] = df['Location'].str.strip()
 df['Company Name'] = df['Company Name'].str.strip()
 
-# Filtrar EUA e China
+
 eua = df[df['Location'].str.contains('USA')]
 china = df[df['Location'].str.contains('China')]
 
-# Empresas únicas
+
 empresas_eua = eua['Company Name'].unique()
 empresas_china = china['Company Name'].unique()
 
-# Quantidade
+
 qtd = [len(empresas_eua), len(empresas_china)]
 paises = ['USA', 'China']
 
-# Gráfico
+
 plt.bar(paises, qtd)
 
 plt.xlabel('País')
